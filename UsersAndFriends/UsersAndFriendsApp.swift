1221dd969bc8ed5bd0efdd416a19ca5a11fba26f
Paul Houghton <paul.houghton@gmail.com>
2020-10-22 09:14:05 +0100

    Initial Commit

---
+++ UsersAndFriends/UsersAndFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersAndFriendsApp.swift
+//  UsersAndFriends
+//
+//  Created by Paul Houghton on 22/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct UsersAndFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
